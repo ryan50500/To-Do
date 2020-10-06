a6b3a0214f0cf3935d292866385cf1a9277e7c6f
ryan woods <federer5050@gmail.com>
2020-10-06 16:07:39 +0100

    adding event listener

---
+++ app.js
@@ -0,0 +1,9 @@
+const addForm = document.querySelector('.add');
+
+addForm.addEventListener('submit', e => {
+
+    e.preventDefault();
+    const todo = addForm.add.value;
+    console.log(todo);
+});
+
